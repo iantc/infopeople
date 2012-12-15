f2fe4b12f0d5890ab2f13aa32f37ee6c2c81d689
Ian Chan <ichan@csusm.edu>
2012-12-14 23:50:38 -0800

    Add Infopeople theming

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+
+// Auto-rebuild the theme registry during theme development.
+if (theme_get_setting('infopeople_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
+  // Rebuild .info data.
+  system_rebuild_theme_data();
+  // Rebuild theme registry.
+  drupal_theme_rebuild();
+}
