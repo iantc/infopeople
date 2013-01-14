55a9c766a64556c8e615de370701458ac9f95a64
Ian Chan <ichan@csusm.edu>
2013-01-14 10:11:24 -0800

    bootstrap and font awesome
    Adjust CSS and JS file includes to utilize minified versions. Add
    stylesheet for Font Awesome icons.

--- template.php
+++ template.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Implements hook_preprocess_page().
+ */
+function infopeople_preprocess_page(&$vars) {
+  $page = $vars['page'];
+
+  // Connect Twitter's Bootstrap framework using Libraries API.
+  if (module_exists('libraries') && $bootstrap_path = libraries_get_path('bootstrap')) {
+    drupal_add_css($bootstrap_path . '/css/bootstrap.min.css', array('media' => 'all'));
+    drupal_add_css($bootstrap_path . '/css/bootstrap-responsive.min.css', array('media' => 'screen'));
+    drupal_add_css($bootstrap_path . '/css/font-awesome.min.css', array('media' => 'screen'));
+    drupal_add_js($bootstrap_path . '/js/bootstrap.min.js');
+  }
+  else {
+    // Notify a user if Bootstrap library was not found.
+    drupal_set_message(t('Bootstrap library was not found. Download it <a href="http://twitter.github.com/bootstrap/">here</a> and put inside <code>sites/all/libraries/</code> folder.'), 'error');
+  }
+}
 // Auto-rebuild the theme registry during theme development.
 if (theme_get_setting('infopeople_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
   // Rebuild .info data.
