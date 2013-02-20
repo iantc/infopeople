868a1ee946a079e74b458248799fd2d79891b639
Ian Chan <ichan@csusm.edu>
2013-02-20 10:02:11 -0800

    Bootstrap libraries, node action links
    Include Boostrap files from libraries folder instead of theme folder
    Change node edit options to use dropdown button menu instead of tabs

--- template.php
+++ template.php
@@ -14,6 +14,10 @@ function infopeople_css_alter(&$css) {
   unset($css[drupal_get_path('theme', 'zenstrap') . '/bootstrap/css/bootstrap.css']);
 }
 
+function infopeople_mymodule_js_alter(&$javascript) {
+  unset($javascript[drupal_get_path('theme', 'zenstrap') . '/bootstrap/js/bootstrap.js']);
+}
+
 function infopeople_preprocess_page(&$vars) {
   $page = $vars['page'];
 
@@ -23,7 +27,7 @@ function infopeople_preprocess_page(&$vars) {
     drupal_add_css($bootstrap_path . '/css/bootstrap.min.css', array('media' => 'all'));
     drupal_add_css($bootstrap_path . '/css/bootstrap-responsive.min.css', array('media' => 'screen'));
     drupal_add_css($bootstrap_path . '/css/font-awesome.min.css', array('media' => 'screen'));
-    //drupal_add_js($bootstrap_path . '/js/bootstrap.min.js');
+    drupal_add_js($bootstrap_path . '/js/bootstrap.min.js');
   }
   else {
     // Notify a user if Bootstrap library was not found.
@@ -38,3 +42,16 @@ if (theme_get_setting('infopeople_rebuild_registry') && !defined('MAINTENANCE_MO
   drupal_theme_rebuild();
 }
 
+function infopeople_menu_local_tasks(&$variables) {
+  $output = '';
+
+  if (!empty($variables['primary'])) {
+    $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables['primary']['#prefix'] = '<div class="btn-group pull-right"><a class="btn dropdown-toggle" data-toggle="dropdown" href="#">Page Options<span class="caret"></span></a>';
+    $variables['primary']['#prefix'] .= '<ul class="dropdown-menu" id="page-tabs">';
+    $variables['primary']['#suffix'] = '</ul></div>';
+    $output .= drupal_render($variables['primary']);
+  }
+
+  return $output;
+}
